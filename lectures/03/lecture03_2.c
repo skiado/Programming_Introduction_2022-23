a888d7b53c586d5aec9ca6f7e23475c392f407f3
skiado <70379186+skiado@users.noreply.github.com>
2022-10-23 17:38:59 +0300

    Add files via upload

---
+++ lectures/03/lecture03_2.c
@@ -0,0 +1,20 @@
+/*Πρόγραμμα υπολογισμού του 200 τριγωνικού αριθμού
+  με χρήση της εντολής for  */
+  
+#include <stdio.h>
+
+int main (){
+	int n , triangularNumber;
+
+	triangularNumber = 0;
+
+	for ( n = 1; n <= 200; n++){
+		triangularNumber = triangularNumber + n;
+	}
+	printf ("Ο διακοσιοστός τριγωνικός αριθμός είναι %i\n", triangularNumber);
+	return 0;
+}
+
+/* gcc lecture03_2.c -o 03_2 */
+
+
